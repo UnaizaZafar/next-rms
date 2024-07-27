6d7eb4d9f1e3fee1e857424dc98fd7a0b74f7b46
UnaizaZafar <unaizaofficial840@gmail.com>
2024-07-27 21:23:16 +0500

    index.js update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import { Inter } from "next/font/google";
 
-
+import Link from "next/link";
 import React from "react";
+import loginPage from './login-page/index'
+import analyticPage from './analytics-page/index'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return;
   <>
-    
+    <Link href={loginPage}> </Link>
+    <Link href={analyticPage}> </Link>
+
+   
   </>;
 }
