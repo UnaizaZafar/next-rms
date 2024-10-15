10248e2c675d69e6bf805e21ffec8f380a07a3c2
UnaizaZafar <unaizaofficial840@gmail.com>
2024-10-15 19:40:33 +0500

    delete icon added

--- components/DeleteModal.js
+++ components/DeleteModal.js
@@ -1,3 +1,5 @@
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 const DeleteModal = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
   const handleClose = (e) => {
@@ -19,17 +21,25 @@ const DeleteModal = ({ isVisible, onClose }) => {
         >
           X
         </button>
-        <div className=" rounded-xl flex flex-col p-6 gap-3 justify-center bg-white w-full">
-          dustbin
+        <div className=" rounded-xl flex flex-col p-6 gap-3 items-center justify-center bg-white w-full">
+          <RiDeleteBin6Line className="bg-[#FFF1F2] rounded-full text-3xl p-1.5"/>
           <h1 className="font-semibold text-lg text-center">Confirm Delete?</h1>
           <p className="text-center text-sm font-normal ">
             This action is irreversible and can’t be undone. Do you wish to
             proceed?
           </p>
           <div className="flex gap-3 items-center justify-center">
-            <button className="rounded-md py-3 px-4 font-semibold text-sm border border-black">Cancel</button>
-            <button className="rounded-md py-3 px-4 font-semibold text-sm bg-[#E11D48] text-white">Yes, Delete Now</button>
-
+            <button
+              className="rounded-md py-3 px-4 font-semibold text-sm border border-black"
+              onClick={() => {
+                onClose();
+              }}
+            >
+              Cancel
+            </button>
+            <button className="rounded-md py-3 px-4 font-semibold text-sm bg-[#E11D48] text-white">
+              Yes, Delete Now
+            </button>
           </div>
         </div>
       </div>
